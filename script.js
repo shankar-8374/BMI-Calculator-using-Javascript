62ac43737fb5ff136767d3f649afb2e4d1bc3ade
Shankar Kumar Suddamalla <suddamallashankarkumar@gmail.com>
2022-09-23 17:32:59 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -4,4 +4,16 @@ function BMI()
 	var height=Number(document.getElementById("height").value);
 	var BMI=weight/height**2;
 	document.getElementById("result").value =BMI;
+	if(BMI<18.5)
+	{
+		window.alert("you seems to be Underweight.");
+	}
+	else if(18.5<BMI<25)
+	{
+		window.alert("Congrats!You are Perfect.");
+	}
+	else if(25<BMI<30)
+	{
+		window.alert("OOPS! you are overweight.")
+	}
 }
